e1d2330c82be7a0ac0c42580a1e03684e883a0d5
Vitaliy Malyshev <Vitaliy.Malyshev@sizmek.com>
2018-12-06 16:23:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Review score
+
+- Setup your config.json from example (you should copy cookie from your browser)
+- Run npm install
+- Run with REPO and AGE arguments (i.e. `node index ui/common 3d` to get ui/common review stats for last 3 days)
+- Wait for results :)
+
+```
+
+Fetching for ui/common by age 3d
+13 gerrits found
+[
+	"John Blackwell: 4,
+	"Alex Moon": 9
+]
+
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+
+const settings = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
+const score = []
+
+const PROJECT = process.argv[2]
+const AGE = process.argv[3]
+
+console.log(`Fetching for ${PROJECT} by age ${AGE}`)
+
+const fetch_data = endpoint => fetch(settings.url + endpoint , {
+	credentials: 'include',
+	method: 'GET',
+	headers: { 'Cookie':  settings.cookie }
+})
+.then(res => res.text())
+.then(text => JSON.parse(text.replace(')]}\'', '')))
+
+const calc_reviews_for_gerrit = gerrit_details => gerrit_details['messages'].reduce((reviewed, message) => {
+	const author = message.author ? message.author.name : false
+	const text = message.message
+	if (settings.team.includes(author) && !reviewed[author] && (text.includes('comment') || text.includes('Code-Review'))) {
+		score[author] = score[author] ? (score[author] + 1) : 1
+		reviewed[author] = true
+	}
+	return reviewed
+}, {})
+
+fetch_data(`/changes/?q=project:${PROJECT}+-age:${AGE}`)
+	.then(response => {
+		console.log(`${response.length} gerrits found`)
+		return response
+			.map(gerrit => `/changes/${gerrit['id']}/detail`)
+			.reduce((p, gerrit) => p.then(() => fetch_data(gerrit).then(calc_reviews_for_gerrit)), Promise.resolve())
+	})
+	.then(() => console.log(score))
